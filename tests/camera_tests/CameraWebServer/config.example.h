77ce83bb8e2d3ed5541500857d5ac70f8d562a41
Eric Palafox <ericpalafox96@gmail.com>
2026-06-29 21:18:40 -0700

    Add ESP32-CAM camera node test

---
+++ tests/camera_tests/CameraWebServer/config.example.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Copy this file to config.h
+// Fill in your own Wi-Fi credentials
+
+const char* WIFI_SSID = "YOUR_WIFI_NAME";
+const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+#endif
